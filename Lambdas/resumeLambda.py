aa9dff3cc629abd61773ab955d43c841662e82f6
binvant <binvantbajwa@gmail.com>
2022-12-02 15:08:54 -0500

    Update resumeLambda.py

--- Lambdas/resumeLambda.py
+++ Lambdas/resumeLambda.py
@@ -8,6 +8,7 @@ def lambda_handler(event, context):
     dynamo_db = boto3.resource('dynamodb')
     dynamo = boto3.client('dynamodb')
     table = dynamo_db.Table('sequence')
+    table2 = boto3.resource('dynamodb').Table('candidate_data')
     print(event)
     ev = event['body-json']
     ev = bytes(ev, 'utf-8')
@@ -17,20 +18,26 @@ def lambda_handler(event, context):
     print(id)
     resume = base64.b64decode(ev)
     bucket = 'resume-bucket-bsb'
-    file_path = '/resume'
+    file_path = 'resume' + id
     s3 = boto3.client('s3')
     s3.put_object(Bucket=bucket, Key=file_path, Body=resume)
     textract = boto3.client('textract')
     doc = textract.detect_document_text(Document={
         'S3Object': {
             'Bucket': bucket,
-            'Name': '/newwwww_candidate_resume',
+            'Name': '/newwwww_candidate_resume', #Change to file_path
         }
     })
     blocks = doc['Blocks']
     for i in range(len(blocks)):
         if "Text" in blocks[i] and blocks[i]['Text'] in SKILLS:
             print(blocks[i+1]['Text'])
+        #     table2.update_item(Key={"id": id},
+        #     UpdateExpression="set skills=:newskills",ExpressionAttributeValues={
+        #     ":newSkills": blocks[i+1]['Text']
+        # },
+        # ReturnValues="UPDATED_NEW")
+            dynamo.put_item(Table="candidate_data",Item={'id':{'N': id}, 'skills': {'S': blocks[i+1]['Text']}})
 
     return {
         'statusCode': 200,
