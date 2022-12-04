7c7d913c368785f930a2d4c016e5371710d0483b
binvant <binvantbajwa@gmail.com>
2022-12-03 20:39:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
 //const fs = require('fs');
 //const AWS = require('aws-sdk');
-
+let temp;
+function convertToBase64(selectedFile) {
+  //Read File
+  //Check File is not Empty
+  console.log(selectedFile)
+  if (selectedFile) {
+    console.log("Inside if")
+      // Select the very first file from list
+      var fileToLoad = selectedFile;
+      // FileReader function for read the file.
+      var fileReader = new FileReader();
+      var base64;
+      // Onload of file read the file content
+      fileReader.onload = function(fileLoadedEvent) {
+        temp = fileLoadedEvent.target.result;
+          // Print data in console
+          console.log(base64);
+      };
+      // Convert data to base64
+      console.log("Filetoload")
+      console.log(fileToLoad)
+      let toReturn = fileReader.readAsDataURL(fileToLoad);
+      console.log("done")
+      console.log(toReturn);
+      return temp;
+  }
+}
 function signup() {
     let name = $('#name').val();
     let email = $('#mail').val();
     let password = $('#password').val();
-    // let myFile = $('#myFile').serialize();
-    // let file = document.getElementById('myFile').files[0];
-    // let formData = new FormData();
-    // formData.append("resume", file, "");
-    /*formData.append("name", "Binvant");
-    formData.append("password", "xyzzzz");
-    formData.append("email", "binvantbajwa@gmail.com");*/
-
-    /*const s3 = new AWS.S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  });*/
-           
-    //formData.append("file", file);
-    /*$.ajax({
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS, POST'
-        },
-        "mimeType": "multipart/form-data",
-        contentType: "application/json",
-        method: "POST",
-        url: "https://jwjkqbwke5.execute-api.us-east-1.amazonaws.com/dev/uploadresume",
-        data: {
-          "resume": formData
-        }
-      }).done(function() {
-        $(this).addClass( "done" );
-      });*/
-
       $.ajax({
         headers: {
             'Access-Control-Allow-Origin': '*',
@@ -55,78 +50,54 @@ function signup() {
       }).done(function() {
         $(this).addClass( "done" );
       });
-      
-      // var settings = {
-      //   headers: {
-      //     'Access-Control-Allow-Origin': '*',
-      //     'Access-Control-Allow-Headers': '*',
-      //     'Access-Control-Allow-Methods': 'OPTIONS, POST',
-      // },
-      //   "url": "https://jwjkqbwke5.execute-api.us-east-1.amazonaws.com/dev/createprofile",
-      //   "method": "POST",
-      //   "timeout": 0,
-      //   "mimeType": "application/json",
-      //   "contentType": "application/",
-      //   "data": JSON.stringify({
-      //     "name": name,
-      //     "email": email,
-      //     "password": password
-      // }),
-      //   "isBase64Encoded": false
-      // };
-      // $.ajax(settings).done(function (response) {
-      //   console.log(response);
-      // });
+      var fd = new FormData();
+      var File = document.getElementById("myFile").files[0];
+      let promise = new Promise((resolve, reject) => {
+        //resolve(convertToBase64(File));
+      //File = convertToBase64(File);
+      convertToBase64(File);
+      //console.log("Just after function return");
+      //console.log(temp);
+      let that = this;
+      setTimeout(() => {
+        //temp = convertToBase64(File);
+        console.log("The file sent in resolve");
+        console.log(temp);
+        resolve(temp);
+      }, 3000);
+      /*if (File){
+        resolve(File);
+      }else {
+        reject(File);
+      }*/ 
+      });
+      promise.then(
+        function() {
+          console.log("Inside resolve");
+          resolveUpload(File);
+        }, 
+        function(Fie){
+          rejectUpload(File);
+        }
+       );
+      }
 
-      /*fs.readFile(fileName, (err, data) => {
-        if (err) throw err;
-        const params = {
-            Bucket: 'testBucket', // pass your bucket name
-            Key: 'contacts.csv', // file will be saved as testBucket/contacts.csv
-            Body: JSON.stringify(data, null, 2)
-        };
-        s3.upload(params, function(s3Err, data) {
-            if (s3Err) throw s3Err
-            console.log(`File uploaded successfully at ${data.Location}`)
-        });
-     });*/
-
-     
-      // var setting_resume = {
-      //   headers: {
-      //     'Access-Control-Allow-Origin': '*',
-      //     'Access-Control-Allow-Headers': '*',
-      //     'Access-Control-Allow-Methods': 'OPTIONS, POST',
-      //     'Content-Type': file.type
-      // },
-      //   "url": "https://jwjkqbwke5.execute-api.us-east-1.amazonaws.com/dev/uploadresume",
-      //   "method": "POST",
-      //   "timeout": 0,
-      //   "processData": false,
-      //   "mimeType": "multipart/form-data",
-      //   "contentType": "application/pdf",
-      //   "data": pdfFile,
-      //   "isBase64Encoded": true
-      // };
-      var fd = new FormData(),
-      File = document.getElementById("myFile").files[0];
-      fd.append( 'file',  File);
-      console.log(File.type)
+    function resolveUpload(File){
+      console.log("Filee")
+      console.log(temp)
       $.ajax({headers:{
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': '*',
         'Access-Control-Allow-Methods': 'OPTIONS, POST',
-        'Content-Type': File.type
     },
-      "url": "https://jwjkqbwke5.execute-api.us-east-1.amazonaws.com/dev/uploadresume",
-      "method": "POST",
-      "mimeType": "application/pdf",
-      "contentType": "application/pdf",
-      "processData": false,
-      "contentType": false,
-      "data": fd,
-      "isBase64Encoded": true
+      url: "https://jwjkqbwke5.execute-api.us-east-1.amazonaws.com/dev/uploadresume",
+      method: "POST",
+      data: temp,
+      isBase64Encoded: false
     }).done(function (response) {
         console.log(response);
       });
+    }
+    function rejectUpload(inp){
+      console.log(inp)
     }
